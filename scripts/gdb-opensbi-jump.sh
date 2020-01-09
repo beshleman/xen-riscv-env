acc45dd4d37ff151d12bb91f95866ebab2cf1ba6
Bobby Eshleman <bobbyeshleman@gmail.com>
2020-01-09 14:50:01 -0600

    update scripts

--- scripts/gdb-opensbi-jump.sh
+++ scripts/gdb-opensbi-jump.sh
@@ -3,10 +3,10 @@
 riscv64-unknown-linux-gnu-gdb \
 	-ex "target remote localhost:1234" \
 	-ex "add-symbol-file xen/xen/xen-syms 0x80200000" \
+        -ex "add-symbol-file opensbi/build/platform/qemu/virt/firmware/fw_jump.elf 0x80000000" \
 	-ex "file xen/xen/xen-syms" \
 	-ex "hbreak *0x80200000" \
 	-ex "continue" \
-        -ex "delete"    \
 	-ex "directory xen/xen/arch/riscv/" \
 	-ex "set logging file /tmp/gdb.txt" \
 	-ex "set logging on" \
--- scripts/opensbi-jump.sh
+++ scripts/opensbi-jump.sh
@@ -2,22 +2,38 @@
 
 set -e
 
-
-FLAGS=" -machine virt -cpu rv64,x-h=true -M virt -m 512M -display none -serial stdio -monitor telnet::45454,server,nowait"
+CROSS_COMPILE=riscv64-unknown-linux-gnu- 
 
 if [[ "$@" =~ "build" ]]; then
     pushd xen/xen
-    XEN_TARGET_ARCH=riscv64 CROSS_COMPILE=riscv64-unknown-linux-gnu- make build -j8
+    XEN_TARGET_ARCH=riscv64 CROSS_COMPILE=riscv64-unknown-linux-gnu- make build -j$(nproc)
     popd
 fi
 
-if [[ "$@" =~ "debug" ]]; then
-    FLAGS="-s -S ${FLAGS}"
+if [[ "$@" =~ "iwyu" ]]; then
+    CROSS_COMPILE_OLD=$CROSS_COMPILE
+    pushd xen/xen
+    set -x
+    CROSS_COMPILE=
+    XEN_TARGET_ARCH=riscv64 make build -k CC=$(which include-what-you-use)
+    set +x
+    popd
+    CROSS_COMPILE=$CROSS_COMPILE_OLD
 fi
 
-echo "+++++++++++++++++++++++++"
-echo "Succesful build, spinning up QEMU...."
-echo "+++++++++++++++++++++++++"
+if [[ "$@" =~ "run" ]]; then
+    FLAGS=" -machine virt -cpu rv64,x-h=true -M virt -m 512M -display none -serial stdio -monitor telnet::45454,server,nowait"
 
-qemu-system-riscv64 ${FLAGS} -kernel opensbi/build/platform/qemu/virt/firmware/fw_jump.elf -device loader,file=xen/xen/xen,addr=0x80200000 -D /tmp/qemu.log
+    if [[ "$@" =~ "debug" ]]; then
+        FLAGS="-s -S ${FLAGS}"
+    fi
+
+    echo "+++++++++++++++++++++++++"
+    echo "Succesful build, spinning up QEMU...."
+    echo "+++++++++++++++++++++++++"
+
+
+    qemu-system-riscv64 ${FLAGS} -kernel opensbi/build/platform/qemu/virt/firmware/fw_jump.elf -device loader,file=xen/xen/xen,addr=0x80200000
+fi
+
 		
