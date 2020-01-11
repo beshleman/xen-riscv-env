767aab7ead9e556e9e8f8516be47ca2a0ffd3871
Bobby Eshleman <bobbyeshleman@gmail.com>
2020-01-10 21:28:58 -0600

    update

--- debug-qemu.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-gdb qemu/riscv64-softmmu/qemu-system-riscv64 
-# r -nographic -machine virt      -kernel xen-on-riscv/riscv-pk/build/bbl -append "root=/dev/vda ro console=ttyS0"      -drive file=xen-on-riscv/busybear-linux/busybear.bin,format=raw,id=hd0      -device virtio-blk-device,drive=hd0
-
-# r -s -S -nographic -machine virt      -kernel xen-on-riscv/riscv-pk/build/bbl -append "root=/dev/vda ro console=ttyS0"      -drive file=xen-on-riscv/busybear-linux/busybear.bin,format=raw,id=hd0      -device virtio-blk-device,drive=hd0
-
-
-# r -s -S -nographic -machine virt  -kernel xen-on-riscv/opensbi/build/platform/qemu/virt/firmware/fw_jump.elf -append "root=/dev/vda ro console=ttyS0"      -drive file=xen-on-riscv/busybear-linux/busybear.bin,format=raw,id=hd0      -device virtio-blk-device,drive=hd0
--- killqemu.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-kill -s 9 $(ps aux | grep qemu | cut -d ' ' -f 4)
