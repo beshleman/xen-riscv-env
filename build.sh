9016e98ffe45fb3e5848ac56d496891437b2a291
Bobby Eshleman <bobbyeshleman@gmail.com>
2020-01-13 09:21:40 -0600

    Update scripts

--- build.sh
+++ build.sh
@@ -3,11 +3,9 @@
 set -e
 
 pushd xen/xen
-#XEN_TARGET_ARCH=riscv64 CROSS_COMPILE=riscv64-linux-gnu- make build -j8
+XEN_TARGET_ARCH=riscv64 CROSS_COMPILE=riscv64-unknown-linux-gnu- make build -j8
 popd
 
 pushd opensbi
 CROSS_COMPILE=riscv64-unknown-linux-gnu- PLATFORM=qemu/virt FW_PAYLOAD_PATH=../xen/xen/xen make -j8
 popd
-
-qemu-system-riscv64 -s -S -cpu rv64,x-h=true -M virt -m 512M -display none -serial stdio -kernel opensbi/build/platform/qemu/virt/firmware/fw_payload.elf
--- container/gdb.sh
+++ container/gdb.sh
@@ -1,4 +1,8 @@
 #!/bin/env bash
 
-riscv64-linux-gnu-gdb \
-	-ex 'target remote 172.19.0.2:1234'
+riscv64-unknown-linux-gnu-gdb \
+	-ex "target remote 172.19.0.2:1234" \
+	-ex "add-symbol-file xen/xen/xen-syms 0x80200000" \
+	-ex "hb *0x80200000" \
+	-ex "file xen/xen/xen-syms" \
+	-ex "c"
