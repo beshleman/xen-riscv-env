5591696c6fe027762fc0102d175f69c146b38c66
Pete Dietl <petedietl@gmail.com>
2020-01-25 11:46:09 -0600

    Change launch.sh to use current Docker version

--- launch.sh
+++ launch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -e
+set -eu
 
 if ! docker network ls | grep -q my-net; then
 	docker network create my-net --subnet 172.19.0.0/24
@@ -10,4 +10,4 @@ docker run --ip 172.19.0.2 \
 	--publish 1234 \
 	--network my-net \
 	-v $(readlink -e .):/home/ \
-	-it petebob/riscv bash
+	-it petebob/riscv:$(make --no-print-directory -C container version) bash
