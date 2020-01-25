1128bbffc42ee46176988411b33dfee178642a04
Bobby Eshleman <bobbyeshleman@gmail.com>
2020-01-25 12:22:18 -0600

    run_docker_net.sh: Add DOCKER_VERSION

--- scripts/run_docker_net.sh
+++ scripts/run_docker_net.sh
@@ -5,6 +5,8 @@ set -eu
 IP=$1
 ENTRY=$2
 
+DOCKER_VERSION=$(make --no-print-directory -C container version)
+
 if ! docker network ls | grep -q my-net; then
 	docker network create my-net --subnet 172.19.0.0/24
 fi
@@ -13,5 +15,4 @@ docker run --ip ${IP} \
 	--publish 1234 \
 	--network my-net \
 	-v $(readlink -e .):/home/ \
-	-it petebob/riscv:$(make --no-print-directory -C container version) \
-		${ENTRY}
+	-it petebob/riscv:${DOCKER_VERSION} ${ENTRY}
