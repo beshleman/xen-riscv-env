07aeb5c78047f920b02bcddc70687bad8e65ee96
Bobby Eshleman <bobbyeshleman@gmail.com>
2020-05-16 20:14:37 -0500

    debug.sh: use TAG for docker

--- debug.sh
+++ debug.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-DOCKER_IMAGE_TAG=registry.gitlab.com/bobbyeshleman/xen/archlinux:riscv
-
 set -eu
 
-./scripts/run_docker_net.sh 172.19.0.3 ./scripts/remote_debug.sh $(make print-DOCKER_IMAGE_VER)
+./scripts/run_docker_net.sh 172.19.0.3 ./scripts/remote_debug.sh $(make print-DOCKER_IMAGE_TAG)
