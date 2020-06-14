11aceb20bde27435b1e686342de467d6f4f5f739
Bobby Eshleman <bobbyeshleman@gmail.com>
2020-06-13 22:05:59 -0700

    run_qemu.sh: add kernel loading address check

--- scripts/run_qemu.sh
+++ scripts/run_qemu.sh
@@ -2,15 +2,20 @@
 
 set -e
 
+SETUP_FILE=xen/xen/arch/riscv/setup.c
+
 DOM0_KERNEL=linux/arch/riscv/boot/Image
 OPENSBI=opensbi/build/platform/qemu/virt/firmware/fw_payload.elf
 
 XEN_OPENSBI_SZ=0x$(printf "%x" $(wc -c ${OPENSBI} | cut -d ' ' -f 1))
 DOM0_KERNEL_LOAD_ADDR=$(scripts/round_page.py ${XEN_OPENSBI_SZ})
-XEN_EXPECTED_DOM0_LOAD_ADDR=$(cat xen/xen/arch/riscv/setup.c | grep DOM0_KERNEL\ 0x)
+XEN_EXPECTED_DOM0_LOAD_ADDR=$(cat -n ${SETUP_FILE} | grep DOM0_KERNEL\ 0x)
 
 [[ ! "${XEN_EXPECTED_DOM0_LOAD_ADDR}" =~ "${DOM0_KERNEL_LOAD_ADDR}" ]] && {
 	echo "The DOM0 kernel load address has changed!" >&2
+	echo "It is ${DOM0_KERNEL_LOAD_ADDR}" >&2
+	echo "Line in ${SETUP_FILE} containing Xen definition:" >&2
+	echo "${XEN_EXPECTED_DOM0_LOAD_ADDR}" >&2
 	echo "Please modify xen (riscv/setup.c) to reflect the change." >&2
 	echo "It will break otherwise!" >&2
 	exit 1
