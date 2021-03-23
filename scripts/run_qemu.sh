0a8c2a771e27ac7284576cd98fd1840ae7b36223
Bobby Eshleman <bobby.eshleman@gmail.com>
2021-03-22 21:00:46 -0700

    run_qemu: bump memory allotted
    Signed-off-by: Bobby Eshleman <bobby.eshleman@gmail.com>

--- scripts/run_qemu.sh
+++ scripts/run_qemu.sh
@@ -4,28 +4,25 @@ set -e
 
 SETUP_FILE=xen/xen/arch/riscv/setup.c
 
-FLASH2=0x22000000
-DOM0_KERNEL=linux/arch/riscv/boot/Image
+DOM0_KERNEL=bins/Image.gz
+DOM0_KERNEL_SIZE=$(cat ${DOM0_KERNEL} | wc -c)
 OPENSBI=opensbi/build/platform/qemu/virt/firmware/fw_payload.elf
 XEN_OPENSBI_SZ=0x$(printf "%x" $(wc -c ${OPENSBI} | cut -d ' ' -f 1))
-DOM0_KERNEL_LOAD_ADDR=${FLASH2}
-XEN_EXPECTED_DOM0_LOAD_ADDR=$(cat -n ${SETUP_FILE} | grep DOM0_KERNEL\ 0x)
-
-[[ ! "${XEN_EXPECTED_DOM0_LOAD_ADDR}" =~ "${DOM0_KERNEL_LOAD_ADDR}" ]] && {
-	echo "The DOM0 kernel load address has changed!" >&2
-	echo "It is ${DOM0_KERNEL_LOAD_ADDR}" >&2
-	echo "Line in ${SETUP_FILE} containing Xen definition:" >&2
-	echo "${XEN_EXPECTED_DOM0_LOAD_ADDR}" >&2
-	echo "Please modify xen (riscv/setup.c) to reflect the change." >&2
-	echo "It will break otherwise!" >&2
-	exit 1
-}
+DOM0_KERNEL_LOAD_ADDR=$(cat ${SETUP_FILE} | grep DOM0_KERNEL\ 0x | cut -d ' ' -f 3)
 
 echo "QEMU will place kernel @ addr: ${DOM0_KERNEL_LOAD_ADDR}"
 
+#EXPECTED_SIZE=$(cat ${SETUP_FILE} | sed -ne 's/#define DOM0_KERNEL_SIZE \(.*\)/\1/p')
+#[[ ! ${EXPECTED_SIZE} -eq ${DOM0_KERNEL_SIZE} ]] && {
+##    echo "kernel size in ${SETUP_FILE} (${EXPECTED_SIZE}) != size of ${DOM0_KERNEL} (${DOM0_KERNEL_SIZE})" >&2
+#    exit 1
+#}
+
 qemu-system-riscv64 \
 	-cpu rv64,x-h=true \
-	-M virt -m 512M -display none \
+	-M virt -m 1G -display none \
+    -smp 1 \
 	-serial stdio \
+    -monitor telnet::45454,server,nowait  \
 	-device loader,file=$(readlink -e ${DOM0_KERNEL}),addr=${DOM0_KERNEL_LOAD_ADDR} \
-	-kernel "$(readlink -e ${OPENSBI})" -s -S #-machine dumpdtb=riscv64-qemu-virt.dtb
+	-kernel "$(readlink -e ${OPENSBI})" -s -S # -machine dumpdtb=riscv64-qemu-virt.dtb
